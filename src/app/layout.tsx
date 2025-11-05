91b9c86e375ea6255ed3663b9c8be3e4ecd50b82
jeffli2002 <jeffli2002@example.com>
2025-11-05 10:26:33 +0800

    feat: add adsense snippet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -86,6 +86,11 @@ export default async function RootLayout({
           content="n43QebBEBeyBsRUaZ4yd5VksyiYaeKzwOAinwhgVSeo"
         />
         <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9378191378774896"
+          crossOrigin="anonymous"
+        />
+        <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
             __html: generateStructuredData([
